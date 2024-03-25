809824de710a742a5354c717729026e12d5f427f
Anton Abyshev <abyshev.anton@gmail.com>
2024-03-25 14:47:25 +0400

    Initial iOS Project

---
+++ ExpenseLineIOS/ExpenseLineIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseLineIOSApp.swift
+//  ExpenseLineIOS
+//
+//  Created by Anton Abyshev on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseLineIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
